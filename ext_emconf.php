82c8db4d57c9308fe214270f04eaa826b7866d49
oliverklee <github@oliverklee.de>
2008-04-13 12:44:55 +0000

    [Bug 1801] Update the requirements to TYPO3 4.0 and PHP 5.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/contactslist/trunk@8886 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "contactslist"
 #
-# Auto generated 13-04-2008 13:26
+# Auto generated 13-04-2008 14:40
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -33,18 +33,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '0.3.0',
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"7803";s:40:"class.tx_contactslist_templatehelper.php";s:4:"e24d";s:12:"ext_icon.gif";s:4:"2d12";s:17:"ext_localconf.php";s:4:"3400";s:14:"ext_tables.php";s:4:"b116";s:14:"ext_tables.sql";s:4:"bb9d";s:24:"ext_typoscript_setup.txt";s:4:"dba0";s:24:"icon_tx_contactslist.gif";s:4:"2d12";s:33:"icon_tx_contactslist_contacts.gif";s:4:"d930";s:16:"locallang_db.xml";s:4:"21d6";s:7:"tca.php";s:4:"6327";s:8:"todo.txt";s:4:"5260";s:14:"doc/manual.sxw";s:4:"23d8";s:33:"pi1/class.tx_contactslist_pi1.php";s:4:"eec0";s:24:"pi1/contactslist_pi1.css";s:4:"b363";s:25:"pi1/contactslist_pi1.tmpl";s:4:"106a";s:17:"pi1/locallang.xml";s:4:"ecf2";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"9fc1";s:40:"class.tx_contactslist_templatehelper.php";s:4:"12f6";s:12:"ext_icon.gif";s:4:"2d12";s:17:"ext_localconf.php";s:4:"3e09";s:14:"ext_tables.php";s:4:"8b82";s:14:"ext_tables.sql";s:4:"b7ab";s:24:"icon_tx_contactslist.gif";s:4:"2d12";s:33:"icon_tx_contactslist_contacts.gif";s:4:"d930";s:16:"locallang_db.xml";s:4:"21d6";s:7:"tca.php";s:4:"c34c";s:8:"todo.txt";s:4:"cdab";s:14:"doc/manual.sxw";s:4:"0d17";s:33:"pi1/class.tx_contactslist_pi1.php";s:4:"2347";s:24:"pi1/contactslist_pi1.css";s:4:"4738";s:25:"pi1/contactslist_pi1.tmpl";s:4:"106a";s:17:"pi1/locallang.xml";s:4:"33f7";s:20:"pi1/static/setup.txt";s:4:"dba0";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
 			'sr_static_info' => '',
-			'php' => '3.0.0-0.0.0',
-			'typo3' => '3.5.0-0.0.0',
+			'php' => '5.1.0-0.0.0',
+			'typo3' => '4.0.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
+	),
+	'suggests' => array(
 	),
 );
 
