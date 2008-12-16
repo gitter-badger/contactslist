b88024aa3043bd01fc29721e2d43e6bdacae1af5
oliverklee <github@oliverklee.de>
2008-12-16 18:37:39 +0000

    Released 0.5.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/contactslist/trunk@14994 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "contactslist"
 #
-# Auto generated 18-04-2008 14:52
+# Auto generated 16-12-2008 19:37
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'oliverklee.de',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.4.0',
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"7679";s:12:"ext_icon.gif";s:4:"2d12";s:17:"ext_localconf.php";s:4:"f901";s:14:"ext_tables.php";s:4:"0d1f";s:14:"ext_tables.sql";s:4:"c1f8";s:24:"icon_tx_contactslist.gif";s:4:"2d12";s:33:"icon_tx_contactslist_contacts.gif";s:4:"d930";s:16:"locallang_db.xml";s:4:"21d6";s:7:"tca.php";s:4:"9f69";s:8:"todo.txt";s:4:"a5e3";s:38:"tests/tx_contactslist_pi1_testcase.php";s:4:"52be";s:14:"doc/manual.sxw";s:4:"3427";s:33:"pi1/class.tx_contactslist_pi1.php";s:4:"adcb";s:24:"pi1/contactslist_pi1.css";s:4:"4738";s:25:"pi1/contactslist_pi1.html";s:4:"222a";s:17:"pi1/locallang.xml";s:4:"33f7";s:24:"pi1/static/constants.txt";s:4:"bd54";s:20:"pi1/static/setup.txt";s:4:"4259";}',
+	'version' => '0.5.0',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"c22e";s:12:"ext_icon.gif";s:4:"2d12";s:17:"ext_localconf.php";s:4:"97d8";s:14:"ext_tables.php";s:4:"6cd5";s:14:"ext_tables.sql";s:4:"7cb3";s:24:"icon_tx_contactslist.gif";s:4:"2d12";s:33:"icon_tx_contactslist_contacts.gif";s:4:"d930";s:16:"locallang_db.xml";s:4:"21d6";s:7:"tca.php";s:4:"78c8";s:8:"todo.txt";s:4:"a5e3";s:38:"tests/tx_contactslist_pi1_testcase.php";s:4:"a739";s:14:"doc/manual.sxw";s:4:"6e79";s:33:"pi1/class.tx_contactslist_pi1.php";s:4:"e3a9";s:24:"pi1/contactslist_pi1.css";s:4:"4738";s:25:"pi1/contactslist_pi1.html";s:4:"222a";s:17:"pi1/locallang.xml";s:4:"33f7";s:24:"pi1/static/constants.txt";s:4:"bd54";s:20:"pi1/static/setup.txt";s:4:"4259";}',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-0.0.0',
